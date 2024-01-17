af07bbf8fe02979c7f6b4105146058508faa85e4
yasoug <ougouisyassine@gmail.com>
2024-01-17 20:46:05 +0000

    task 0 : commit 1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- This script creates a table users
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(225) NOT NULL UNIQUE,
+	name VARCHAR(225)
+)
---
+++ 0x00-MySQL_Advanced/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- This script creates the database holberton in my MySQL server
+-- should not fail if the database holberton already exists
+CREATE DATABASE IF NOT EXISTS holberton;
