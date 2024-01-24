5d7047de1b1a61ee3c77c5c94fe4fb567ef72faf
yasoug <ougouisyassine@gmail.com>
2024-01-24 23:29:35 +0000

    task 1 : commit 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module for the Redis NoSQL data storage"""
-from typing import Union
+from typing import Union, Callable
 import redis
 from uuid import uuid4
 
@@ -17,3 +17,17 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Retrieve data from the cache using the specified key"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """Retrieve a string from the cache using the specified key"""
+        return str(self._redis.get(key))
+
+    def get_int(self, key: str) -> int:
+        """Retrieve an integer from the cache using the specified key"""
+        return int(self._redis.get(key))
